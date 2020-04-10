5ff66421959d1410d467d623b227ac142d4c3291
Johannes Møgster <johannes.mogster@mojotech.no>
2020-04-10 20:31:58 +0200

    two plots, double fun

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import CanvasJSReact from './canvasjs/canvasjs.react';
 import './App.css';
 
 let CanvasJSChart = CanvasJSReact.CanvasJSChart;
-let dataPointsS = [];
-let dataPointsI = [];
-let dataPointsR = [];
+let dataPointsS_sir = [];
+let dataPointsI_sir = [];
+let dataPointsR_sir = [];
+let dataPointsS_seir = [];
+let dataPointsE_seir = [];
+let dataPointsI_seir = [];
+let dataPointsR_seir = [];
 
 class App extends Component {
 
   render() {
-    const options = {
+    const options_sir = {
       theme: "light2",
       title: {
         text: "SIR model"
@@ -21,37 +25,89 @@ class App extends Component {
         includeZero: true
       },
       data: [
-        {
-          type: "line",
-          name: "S",
-          showInLegend: true,
-          xValueFormatString: "day #0",
-          yValueFormatString: "#,####0.0000",
-          dataPoints: dataPointsS
-        },
-        {
-          type: "line",
-          name: "I",
-          showInLegend: true,
-          xValueFormatString: "day #0",
-          yValueFormatString: "#,####0.0000",
-          dataPoints: dataPointsI
-        },
-        {
-          type: "line",
-          name: "R",
-          showInLegend: true,
-          xValueFormatString: "day #0",
-          yValueFormatString: "#,####0.0000",
-          dataPoints: dataPointsR
-        },
+          {
+              type: "line",
+              name: "S",
+              showInLegend: true,
+              xValueFormatString: "day #0",
+              yValueFormatString: "#,####0.0000",
+              dataPoints: dataPointsS_sir
+          },
+          {
+              type: "line",
+              name: "I",
+              showInLegend: true,
+              xValueFormatString: "day #0",
+              yValueFormatString: "#,####0.0000",
+              dataPoints: dataPointsI_sir
+          },
+          {
+              type: "line",
+              name: "R",
+              showInLegend: true,
+              xValueFormatString: "day #0",
+              yValueFormatString: "#,####0.0000",
+              dataPoints: dataPointsR_sir
+          },
 
       ]
     }
+
+      const options_seir = {
+          theme: "light2",
+          title: {
+              text: "SEIR model"
+          },
+          axisY: {
+              title: "Fraction of population",
+              prefix: "",
+              includeZero: true
+          },
+          data: [
+              {
+                  type: "line",
+                  name: "S",
+                  showInLegend: true,
+                  xValueFormatString: "day #0",
+                  yValueFormatString: "#,####0.0000",
+                  dataPoints: dataPointsS_seir
+              },
+              {
+                  type: "line",
+                  name: "E",
+                  showInLegend: true,
+                  xValueFormatString: "day #0",
+                  yValueFormatString: "#,####0.0000",
+                  dataPoints: dataPointsE_seir
+              },
+              {
+                  type: "line",
+                  name: "I",
+                  showInLegend: true,
+                  xValueFormatString: "day #0",
+                  yValueFormatString: "#,####0.0000",
+                  dataPoints: dataPointsI_seir
+              },
+              {
+                  type: "line",
+                  name: "R",
+                  showInLegend: true,
+                  xValueFormatString: "day #0",
+                  yValueFormatString: "#,####0.0000",
+                  dataPoints: dataPointsR_seir
+              },
+
+          ]
+      }
+
     return (
         <div>
-          <CanvasJSChart options = {options}
-                         onRef={ref => this.chart = ref}
+          <CanvasJSChart options = {options_sir}
+                         onRef={ref => this.chart_sir = ref}
+          />
+          {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+          <CanvasJSChart options = {options_seir}
+                         onRef={ref => this.chart_seir = ref}
           />
           {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
         </div>
@@ -59,7 +115,8 @@ class App extends Component {
   }
 
   componentDidMount(){
-    let chart = this.chart;
+    let chart_sir = this.chart_sir;
+    let chart_seir = this.chart_seir;
 
     fetch('http://127.0.0.1:5000/get_corona_data', {
       method: 'POST',
@@ -68,6 +125,7 @@ class App extends Component {
       },
       body: JSON.stringify(
           {
+            model_type: 'SIR',
             username: 'john',
             password: 'hello',
             total_population: 5000000,
@@ -83,21 +141,65 @@ class App extends Component {
     })
         .then(function(data) {
           for (let i = 0; i < data.length; i++) {
-            dataPointsS.push({
+            dataPointsS_sir.push({
               x: data[i]['t'],
               y: data[i]['S'],
             });
-            dataPointsI.push({
+            dataPointsI_sir.push({
               x: data[i]['t'],
               y: data[i]['I'],
             });
-            dataPointsR.push({
+            dataPointsR_sir.push({
               x: data[i]['t'],
               y: data[i]['R'],
             });
           }
-          chart.render();
+            chart_sir.render();
         });
+
+    fetch('http://127.0.0.1:5000/get_corona_data', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+          {
+              model_type: 'SEIR',
+              username: 'john',
+              password: 'hello',
+              total_population: 5000000,
+              duration_days: 365,
+              timestep_days: 1,
+              alpha: 0.2,
+              beta: 1.75,
+              gamma: 0.5,
+              rho: 0.7,
+              social_distancing: false
+          })
+    }).then(function(response) {
+      return response.json();
+    })
+      .then(function(data) {
+          for (let i = 0; i < data.length; i++) {
+              dataPointsS_seir.push({
+                  x: data[i]['t'],
+                  y: data[i]['S'],
+              });
+              dataPointsE_seir.push({
+                  x: data[i]['t'],
+                  y: data[i]['E'],
+              });
+              dataPointsI_seir.push({
+                  x: data[i]['t'],
+                  y: data[i]['I'],
+              });
+              dataPointsR_seir.push({
+                  x: data[i]['t'],
+                  y: data[i]['R'],
+              });
+          }
+          chart_seir.render();
+      });
   }
 }
 
