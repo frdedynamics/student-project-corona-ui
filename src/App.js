f1fe760e289a6913ca560dbfb314711f39478462
Johannes Møgster <johannes.mogster@mojotech.no>
2020-05-03 00:01:39 +0200

    something to play with

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ let dataPointsI_seir = [];
 let dataPointsR_seir = [];
 
 class App extends Component {
-
   render() {
     const options_sir = {
       theme: "light2",
@@ -118,94 +117,132 @@ class App extends Component {
     let chart_sir = this.chart_sir;
     let chart_seir = this.chart_seir;
 
-    // Replace this test token with something better.
-    let my_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODg0MjgwMjEsImlhdCI6MTU4ODQxNzIyMSwibmJmIjoxNTg4NDE3MjIxLCJpZGVudGl0eSI6MX0.mhyAy3fVOyLOG8nhMkHCCL2VXsRicyEl1JON-ekAflo';
+
+    let my_token = '';
 
     let header = new Headers();
     header.append('Content-Type', 'application/json');
-    header.append('Authorization', `JWT ${my_token}`);
 
-    let sir_endpoint = 'http://127.0.0.1:5000/model/SIR';
-    let seir_endpoint = 'http://127.0.0.1:5000/model/SEIR';
 
-    fetch(sir_endpoint, {
-      method: 'POST',
-      headers: header,
-      body: JSON.stringify(
-          {
-            model_type: 'SIR',
-            username: 'john',
-            password: 'hello',
-            total_population: 5000000,
-            I_0: 1000,
-            R_0: 0,
-            average_number_of_people_infected_per_day_per_person: 0.2,
-            average_days_sick_per_person: 7,
-            duration_days: 365,
-            timestep_days: 1
-          })
-    }).then(function(response) {
-      return response.json();
-    })
-        .then(function(data) {
-          for (let i = 0; i < data['model'].length; i++) {
-            dataPointsS_sir.push({
-              x: data['model'][i]['t'],
-              y: data['model'][i]['S'],
-            });
-            dataPointsI_sir.push({
-              x: data['model'][i]['t'],
-              y: data['model'][i]['I'],
-            });
-            dataPointsR_sir.push({
-              x: data['model'][i]['t'],
-              y: data['model'][i]['R'],
-            });
+    let sir_endpoint = 'https://studentprojectcorona.herokuapp.com/model/SIR';
+    let seir_endpoint = 'https://studentprojectcorona.herokuapp.com/model/SEIR';
+
+      fetch('https://studentprojectcorona.herokuapp.com/users', {
+          method: 'GET'
+      }).then(function (response) {
+          return response.json();
+      }).then(function (data) {
+          if (data['users'].length === 0 || data['users'][0]['user'] !== 'john') {
+              console.log("no johnny. do a refresh..")
+              fetch('https://studentprojectcorona.herokuapp.com/register', {
+                  method: 'POST',
+                  headers: {'Content-Type': 'application/json'},
+                  body: JSON.stringify(
+                      {
+                          "username": "john",
+                          "password": "hello"
+                      }
+                  )
+              }).then(function (response) {
+                  return response.json();
+              }).then(function (data) {
+                  console.log(data)
+              });
+          } else {
+              console.log("heeeere`s johnny")
+              fetch('https://studentprojectcorona.herokuapp.com/auth', {
+                  method: 'POST',
+                  headers: {'Content-Type': 'application/json'},
+                  body: JSON.stringify(
+                      {
+                          "username": "john",
+                          "password": "hello"
+                      }
+                  )
+              }).then(function (response) {
+                  return response.json();
+              }).then(function (data) {
+                  my_token = data['access_token']
+                  header.append('Authorization', `JWT ${my_token}`);
+                  fetch(sir_endpoint, {
+                      method: 'POST',
+                      headers: header,
+                      body: JSON.stringify(
+                          {
+                              total_population: 5000000,
+                              I_0: 1000,
+                              R_0: 0,
+                              average_number_of_people_infected_per_day_per_person: 0.2,
+                              average_days_sick_per_person: 7,
+                              duration_days: 365,
+                              timestep_days: 1
+                          })
+                  }).then(function(response) {
+                      return response.json();
+                  })
+                      .then(function(data) {
+                          for (let i = 0; i < data['model'].length; i++) {
+                              dataPointsS_sir.push({
+                                  x: data['model'][i]['t'],
+                                  y: data['model'][i]['S'],
+                              });
+                              dataPointsI_sir.push({
+                                  x: data['model'][i]['t'],
+                                  y: data['model'][i]['I'],
+                              });
+                              dataPointsR_sir.push({
+                                  x: data['model'][i]['t'],
+                                  y: data['model'][i]['R'],
+                              });
+                          }
+                          chart_sir.render();
+                      });
+
+                  fetch(seir_endpoint, {
+                      method: 'POST',
+                      headers: header,
+                      body: JSON.stringify(
+                          {
+                              total_population: 5000000,
+                              duration_days: 365,
+                              timestep_days: 1,
+                              alpha: 0.2,
+                              beta: 1.75,
+                              gamma: 0.5,
+                              rho: 0.7,
+                              social_distancing: false
+                          })
+                  }).then(function(response) {
+                      return response.json();
+                  })
+                      .then(function(data) {
+                          for (let i = 0; i < data['model'].length; i++) {
+                              dataPointsS_seir.push({
+                                  x: data['model'][i]['t'],
+                                  y: data['model'][i]['S'],
+                              });
+                              dataPointsE_seir.push({
+                                  x: data['model'][i]['t'],
+                                  y: data['model'][i]['E'],
+                              });
+                              dataPointsI_seir.push({
+                                  x: data['model'][i]['t'],
+                                  y: data['model'][i]['I'],
+                              });
+                              dataPointsR_seir.push({
+                                  x: data['model'][i]['t'],
+                                  y: data['model'][i]['R'],
+                              });
+                          }
+                          chart_seir.render();
+                      });
+              });
           }
-            chart_sir.render();
-        });
-
-    fetch(seir_endpoint, {
-      method: 'POST',
-      headers: header,
-      body: JSON.stringify(
-          {
-              model_type: 'SEIR',
-              username: 'john',
-              password: 'hello',
-              total_population: 5000000,
-              duration_days: 365,
-              timestep_days: 1,
-              alpha: 0.2,
-              beta: 1.75,
-              gamma: 0.5,
-              rho: 0.7,
-              social_distancing: false
-          })
-    }).then(function(response) {
-      return response.json();
-    })
-      .then(function(data) {
-          for (let i = 0; i < data['model'].length; i++) {
-              dataPointsS_seir.push({
-                  x: data['model'][i]['t'],
-                  y: data['model'][i]['S'],
-              });
-              dataPointsE_seir.push({
-                  x: data['model'][i]['t'],
-                  y: data['model'][i]['E'],
-              });
-              dataPointsI_seir.push({
-                  x: data['model'][i]['t'],
-                  y: data['model'][i]['I'],
-              });
-              dataPointsR_seir.push({
-                  x: data['model'][i]['t'],
-                  y: data['model'][i]['R'],
-              });
-          }
-          chart_seir.render();
       });
+
+
+
+
   }
 }
 
